18be0f340ade199da8504840737a1af4c0554e60
Marie E <marrrielle.e@gmail.com>
2022-07-15 13:59:53 +0300

    вывели игровое поле в виде строки

--- src/View.js
+++ src/View.js
@@ -1,12 +1,12 @@
 // Сделаем отдельный класс для отображения игры в консоли.
 
 class View {
-  render() {
+  render(arrTrack) {
     const yourTeamName = 'Elbrus';
 
     // Тут всё рисуем.
     console.clear();
-    console.log(this.game.track.join(''));
+    console.log(arrTrack.join(''));
     console.log('\n\n');
     console.log(`Created by "${yourTeamName}" with love`);
   }
